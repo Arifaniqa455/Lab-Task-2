ad6c253a1465ee68fb3766cee70cfe5789c71b5b
Arifaniqa455 <chaudharyaniqa77@gmail.com>
2025-09-25 12:12:03 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+class program
+{
+    static void Main()
+    {
+        string line = "----------";
+        int length = line.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("\r" + new string(' ', i) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200);
+            Console.Write("\r" + new string(' ', i + 1) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200);
+        }
+        Console.Write("\r" + new string(' ', length) + "c");
+        Console.WriteLine("\n\n Game over!b Pac Man ate all the dots!");
+
+
+    }
+}
